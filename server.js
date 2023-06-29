5ce868ef236b666e9f6b08528685f457b542e89b
Subrata-Rudra <rudrasubrata27@gmail.com>
2023-06-29 19:47:49 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.set('trust proxy', true)
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("Server is running successfully🎉")
+})
+
+app.get('/ip', (req, res) => {
+    const ip = req.ip
+    res.json({"IP_Address": ip})
+    // res.send(ip)
+})
+
+const port = process.env.POST || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
